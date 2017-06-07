40a7142238db2a7123a92961cd8c5faa320ac7e6
ryansear <Ryan@Ryans-MacBook-Pro-2.local>
2017-06-07 07:43:28 -0700

    updated app icon
    Put new app icon in

--- android/app/build/generated/source/r/release/com/dogpark/R.java
+++ android/app/build/generated/source/r/release/com/dogpark/R.java
@@ -2008,8 +2008,9 @@ containing a value of this type.
         public static final int support_simple_spinner_dropdown_item=0x7f040027;
     }
     public static final class mipmap {
-        public static final int grass=0x7f030000;
-        public static final int ic_launcher=0x7f030001;
+        public static final int dog=0x7f030000;
+        public static final int grass=0x7f030001;
+        public static final int ic_launcher=0x7f030002;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f070000;
